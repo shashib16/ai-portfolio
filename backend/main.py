c6c0259075601742bdc1467d6ef522a6fb280e1e
shashib16 <dev.shashib16@gmail.com>
2025-05-17 15:23:43 +0530

    fixed

--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from api import talk
 from api import ask
 app = FastAPI()  # <- this must exist
-
+# uvicorn main:app --reload
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://shashi-aiportfolio.netlify.app', "*"],
+    allow_origins=['https://shashi-portfolio.netlify.app', . "http://localhost:5173"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allowed_methods=["*"],
+    allow_headers=["*"]
 )
 app.include_router(talk.router, prefix="/api", tags=["api"])
 app.include_router(ask.router, prefix="/api", tags=["api"])
